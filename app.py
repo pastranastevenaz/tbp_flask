11b4594a2208be6c0057d63b93553d1458c72735
Steven Antonio <steven@stevenanton.io>
2017-08-16 01:20:28 -0700

    removed bp code2

--- app.py
+++ app.py
@@ -1,18 +1,27 @@
-from flask import Flask
-from datetime import datetime
+from flask import *
 app = Flask(__name__)
-
 @app.route('/')
-def index():
-    # the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
+def home():
+    return render_template('home.html')
 
-    return """
-    <h1>Hello heroku</h1>
-    """
+@app.route('/welcome')
+def welcome():
+    return render_template('welcome.html]')
+
+@app.route('/welcome', methods=['GET','POST'])
+def welcome():
+    return render_template('welcome.html')
+
+@app.route('/log', methods=['GET', 'POST'])
+def log():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            return redirect(url_for('welcome'))
+    return render_template('log.html', error=error)
 
 if __name__ == '__main__':
-    app.run(debug=True, use_reloader=True)
-
+    app.run()
 
 
 # quarks = [{'name': 'up', 'charge': '+2/3'},
