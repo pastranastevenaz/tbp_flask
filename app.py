fc744293091c146940b260742fb099d253f866b5
Steven Antonio <steven@stevenanton.io>
2017-08-21 22:21:22 -0700

    database done, req update

--- app.py
+++ app.py
@@ -1,14 +1,62 @@
 import os
 import requests
 import json
+from datetime import datetime
 import sys
 import yagmail
+import os
 from flask.json import jsonify
-from flask import Flask, request, render_template, url_for, flash, redirect
-
+from flask import Flask, request, render_template, url_for, flash, redirect, session, abort
+from flask_sqlalchemy  import SQLAlchemy
+import pymysql
+pymysql.install_as_MySQLdb()
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://admin:Qwer!234@localhost:3306/project1'
+db = SQLAlchemy(app)
 app.secret_key = 'some_secret'
+
+class User(db.Model):
+    id=db.Column(db.Integer, primary_key=True)
+    username=db.Column(db.String(80), unique=True)
+    email=db.Column(db.String(120), unique=True)
+
+    def __init__(self, username, email):
+        self.username=username
+        self.email=email
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+class Post(db.Model):
+    id=db.Column(db.Integer, primary_key=True)
+    title=db.Column(db.String(80))
+    body=db.Column(db.Text)
+    pub_date=db.Column(db.DateTime)
+
+    category_id=db.Column(db.Integer, db.ForeignKey('category.id'))
+    category=db.relationship('Category', backref=db.backref('posts', lazy='dynamic'))
+
+    def __init__(self, title, body, category, pub_date=None):
+        self.title=title
+        self.body=body
+        self.category=category
+        if pub_date is None:
+            pub_date = datetime.utcnow()
+        self.pub_date=pub_date
+
+    def __repr__(self):
+        return '<Post %r>' % self.title
+
+class Category(db.Model):
+    id=db.Column(db.Integer, primary_key=True)
+    name=db.Column(db.String(50))
+
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return '<Category %r>' % self.name
 
 
 @app.route("/")
@@ -165,9 +213,19 @@ def feedback():
     elif request.method == 'GET':
         return render_template('feedback.html')
     else: return render_template('error.html')
+
+@app.route("/products", methods=['POST', 'GET', 'DELETE'])
+def products():
+    return "The products GEet"
+
+@app.route("/products-login", methods=['POST', 'GET'])
+def productslogin():
+    return "The login page"
+
 @app.route("/echo", methods=['POST'])
 def echo():
     return render_template('echo.html', text=request.form['text'])
+
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 33507))
